5db145e5dca0113afa6fd4a7c2dbca7b3bf1b857
Juan Diego <juan.sanchez@cerebapp.com>
2017-02-01 19:48:11 -0500

    Proye

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+$('.button-collaps').sideNav({
+ // Default is 300
+
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+
+  }
+);
+
+$(document).ready(function() {
+   $('select').material_select();
+    $('ul.tabs').tabs();
+ });
